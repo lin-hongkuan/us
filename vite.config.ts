0a04c8b984d7b478d516f78bb43f1295ad6a2ebc
lynn401150 <lin2068192798@gmail.com>
2025-12-10 18:18:26 +0800

    2

--- vite.config.ts
+++ vite.config.ts
@@ -39,12 +39,14 @@ export default defineConfig(({ mode }) => {
               {
                 src: 'icon.png',
                 sizes: '192x192',
-                type: 'image/png'
+                type: 'image/png',
+                purpose: 'any maskable'
               },
               {
                 src: 'icon.png',
                 sizes: '512x512',
-                type: 'image/png'
+                type: 'image/png',
+                purpose: 'any maskable'
               }
             ]
           }
