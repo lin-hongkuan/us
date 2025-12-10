0295b083e2a94d276c701d3c7c6fd6d90354fd9e
lynn401150 <lin2068192798@gmail.com>
2025-12-10 19:54:04 +0800

    readme

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5epa8s0q79"
+    "revision": "0.pak283ju6ao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
